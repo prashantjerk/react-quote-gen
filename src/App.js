65f69b378fc3df258ad1758fe2f8bf388d67f985
Prashant <prashantkarki630@gmail.com>
2023-05-01 14:56:27 +0545

    logic added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import "./App.css";
 
 function App() {
   const [quote, setQuote] = useState("");
-
+  const [author, setAuthor] = useState("");
+  let url = `https://type.fit/api/quotes`;
   async function generate() {
-    const response = await fetch("https://zenquotes.io/api/random");
+    const response = await fetch(url);
     const data = await response.json();
-    setQuote(data[0]);
+    console.log(data.length)
+    const randomIndex = Math.floor(Math.random() * data.length);
+    setQuote(data[randomIndex].text);
+    setAuthor(data[randomIndex].author);
+    console.log(data[randomIndex].text);
   }
   useEffect(() => {
     generate();
@@ -20,7 +25,7 @@ function App() {
     <div className="main">
       <div className="quote-generator">
         <button onClick={submitRequest}>Generate Quote</button>
-        {quote}
+        <p className="quote-block">{quote}<br/>{author}</p>
       </div>
     </div>
   );
