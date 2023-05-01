24290d46e61472a14d9ea7e6296696e4c72c7c90
Prashant <prashantkarki630@gmail.com>
2023-05-01 22:02:55 +0545

    styled

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ function App() {
   const [quote, setQuote] = useState("");
   const [author, setAuthor] = useState("");
   let url = `https://type.fit/api/quotes`;
+
   async function generate() {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.length)
+    console.log(data.length);
     const randomIndex = Math.floor(Math.random() * data.length);
     setQuote(data[randomIndex].text);
     setAuthor(data[randomIndex].author);
-    console.log(data[randomIndex].text);
   }
+
   useEffect(() => {
     generate();
   }, []);
@@ -21,11 +22,17 @@ function App() {
   function submitRequest() {
     generate();
   }
+
   return (
     <div className="main">
-      <div className="quote-generator">
-        <button onClick={submitRequest}>Generate Quote</button>
-        <p className="quote-block">{quote}<br/>{author}</p>
+      <div className="hero">
+        <p className="quote-block">
+          {quote}
+          <br />- {author}
+        </p>
+        <button onClick={submitRequest} className="generate-btn">
+          Generate Quote
+        </button>
       </div>
     </div>
   );
